38b8c4934075adee122186367b046a4a38e8d62b
Oliver Heering <heering@riege.com>
2016-11-21 08:36:14 +0100

    fix typo

--- alarms/alarm_manager.py
+++ alarms/alarm_manager.py
@@ -64,8 +64,8 @@ class Alarm_Manager(Thread):
 					set_optional_args(str(alarm))
 				else:
 					log.info("Alarm not activated: " + alarm['type'] + " because value not set to \"True\"")
-	
-	#Update this object with a list of pokemon 
+
+	#Update this object with a list of pokemon
 	def set_pokemon(self, settings):
 		pokemon = {}
 		default_dist = float(settings.pop('max_dist', None) or 'inf');
@@ -91,10 +91,10 @@ class Alarm_Manager(Thread):
 						"move_1": info.get("move_1", 'all'),
 						"move_2": info.get("move_2", 'all')
 					}
-				except Exception as e: 
+				except Exception as e:
 					log.debug("%s error has occured trying to set Pokemon %s" % (str(e), id))
 		self.pokemon_list = pokemon
-	
+
 	#Update data about this request
 	def update(self, id, info):
 		self.lock.acquire()
@@ -104,8 +104,8 @@ class Alarm_Manager(Thread):
 			self.data[id] = info #update info if changed
 		finally:
 			self.lock.release()
-	
-	#Threaded loop to process request data from the queue 
+
+	#Threaded loop to process request data from the queue
 	def run(self):
 		log.info("PokeAlarm has started! Your alarms should trigger now.")
 		while True:
@@ -124,7 +124,7 @@ class Alarm_Manager(Thread):
 						log.debug("Request processing for Pokemon #%s" % data['message']['pokemon_id'])
 						self.trigger_pokemon(data['message'])
 						log.debug("Finished processing for Pokemon #%s" % data['message']['pokemon_id'])
-					elif data['type'] == 'pokestop' : 
+					elif data['type'] == 'pokestop' :
 						log.debug("Request processing for Pokestop %s" % data['message']['pokestop_id'])
 						self.trigger_pokestop(data['message'])
 						log.debug("Finished processing for Pokestop %s" % data['message']['pokestop_id'])
@@ -141,30 +141,30 @@ class Alarm_Manager(Thread):
 				log.debug("Stack trace: \n {}".format(traceback.format_exc()))
 				if data:
 					log.debug("Request format: \n %s " % json.dumps(data, indent=4, sort_keys=True))
-					
+
 	#Send a notification to alarms about a found pokemon
 	def trigger_pokemon(self, pkmn):
 		#If already alerted, skip
 		if pkmn['encounter_id'] in self.pokemon:
 			return
-			
+
 		#Mark the pokemon as seen along with exipre time
 		dissapear_time = datetime.utcfromtimestamp(pkmn['disappear_time']);
 		self.pokemon[pkmn['encounter_id']] = dissapear_time
 		pkmn_id = pkmn['pokemon_id']
 		name = get_pkmn_name(pkmn_id)
-		
+
 		#Check if Pokemon has any filters set up
 		filter = self.pokemon_list.get(pkmn_id)
 		if filter is None:
 			log.info(name + " ignored: notify not enabled.")
 			return
-		
+
 		#Check if the Pokemon has already expired
 		now_time = datetime.utcnow()
 		seconds_left = (dissapear_time - now_time).total_seconds()
 		if seconds_left < config['TIME_LIMIT'] :
-			log.info(name + " ignored: not enough time remaining. disappear: %s now: %s" % (str(disappear_time), str(now_time)))
+			log.info(name + " ignored: not enough time remaining. disappear: %s now: %s" % (str(dissapear_time), str(now_time)))
 			log.debug("Time left must be %f, but was %f." % (config['TIME_LIMIT'], seconds_left))
 			return
 
@@ -176,14 +176,14 @@ class Alarm_Manager(Thread):
 		if filter.get('min_iv') > float(iv):
 			log.info("%s ignored: IV was %f (needs to be %f)" % (name, iv, filter.get('min_iv')))
 			return
-		
+
 		#Check moveset
 		move1 = get_pkmn_move(pkmn.get('move_1', "none"))
 		move2 = get_pkmn_move(pkmn.get('move_2', "none"))
 		if move1 != "unknown" and filter.get('move_1') != 'all' and filter.get('move_1').find(move1) == -1:
 			log.info("%s ignored: Incorrect Move_1 (%s)" %(name, move1))
 			return
-			
+
 		if move2 != "unknown" and filter.get('move_2') != 'all' and filter.get('move_2').find(move2) == -1:
 			log.info("%s ignored: Incorrect Move_2 (%s)" %(name, move2))
 			return
@@ -198,7 +198,7 @@ class Alarm_Manager(Thread):
 			log.info(name + " ignored: outside range")
 			log.debug("Pokemon must be less than %d, but was %d." % (filter['max_dist'], dist))
 			return
-        
+
 		#Check if the Pokemon is in the geofence
 		if 'GEOFENCE' in config:
 			if config['GEOFENCE'].contains(lat,lng) is not True:
@@ -239,25 +239,25 @@ class Alarm_Manager(Thread):
 		#Check if stop is lured or not
 		if stop['lure_expiration'] is None:
 			return
-		
+
 		#If Lures are not enabled
 		if self.stop_list.get('lured') is None:
 			return
-			
+
 		#If already alerted, skip
 		id = stop['pokestop_id']
 		dissapear_time = datetime.utcfromtimestamp(stop['lure_expiration'])
 		if id in self.pokestops and self.pokestops[id] == dissapear_time:
 			return
 		self.pokestops[id] = dissapear_time
-		
+
 		#Check if the Pokestop has already expired
 		seconds_left = (dissapear_time - datetime.utcnow()).total_seconds()
 		if seconds_left < config['TIME_LIMIT'] :
 			log.info("Pokestop ignored: not enough time remaining.")
 			log.debug("Time left must be %f, but was %f." % (config['TIME_LIMIT'], seconds_left))
 			return
-		
+
 		#Check if the Pokestop is outside of notify range
 		lat = stop['latitude']
 		lng = stop['longitude']
@@ -266,7 +266,7 @@ class Alarm_Manager(Thread):
 			log.info("Pokestop ignored: outside range")
 			log.debug("Pokestop must be less than %d, but was %d." % (self.stop_list['lured'], dist))
 			return
-		
+
 		#Check if the Pokestop is in the geofence
 		if 'GEOFENCE' in config:
 			if config['GEOFENCE'].contains(lat,lng) is not True:
@@ -289,24 +289,24 @@ class Alarm_Manager(Thread):
 		}
 
 		stop_info = self.optional_arguments(stop_info)
-		
+
 		for alarm in self.alarms:
 			alarm.pokestop_alert(stop_info)
 
-	
+
 	#Send a notifcation about pokemon gym detected
 	def trigger_gym(self, gym):
 		id = gym.get('gym_id', gym.get('id'))
 		old_team = self.gyms.get(id)
-		new_team = gym.get('team_id', gym.get('team')) 	
+		new_team = gym.get('team_id', gym.get('team'))
 		self.gyms[id] = new_team
 		log.debug("Gym %s - %s to %s" % (id, old_team, new_team))
-		
-		#Check to see if the gym has changed 
+
+		#Check to see if the gym has changed
 		if old_team == None or new_team == old_team:
 			log.debug("Gym ignored: no change detected")
 			return #ignore neutral for now
-		
+
 		#Check for Alert settings
 		old_team = get_team_name(old_team)
 		new_team = get_team_name(new_team)
@@ -314,7 +314,7 @@ class Alarm_Manager(Thread):
 		if max_dist is -1:
 			log.info("Gym ignored: alert not set")
 			return
-			
+
 		#Check if the Gym is outside of notify range
 		lat = gym['latitude']
 		lng = gym['longitude']
@@ -323,13 +323,13 @@ class Alarm_Manager(Thread):
 			log.info("Gym ignored: outside range")
 			log.debug("Gym must be less than %d, but was %d." % (max_dist, dist))
 			return
-		
+
 		#Check if the Gym is in the geofence
 		if 'GEOFENCE' in config:
 			if config['GEOFENCE'].contains(lat,lng) is not True:
 				log.info("Gym ignored: outside geofence")
 				return
-		
+
 		#Trigger the notifcations
 		log.info("Gym notication was triggered!")
 		gym_info = {
@@ -344,10 +344,10 @@ class Alarm_Manager(Thread):
 			'new_team': new_team
 		}
 		gym_info = self.optional_arguments(gym_info)
-		
+
 		for alarm in self.alarms:
 			alarm.gym_alert(gym_info)
-		
+
 	#clear expired pokemon so that the seen set is not too large
 	def clear_stale(self):
 		for dict in (self.pokemon, self.pokestops):
@@ -357,7 +357,7 @@ class Alarm_Manager(Thread):
 					old.append(id)
 			for id in old:
 				del dict[id]
-	
+
 	#clear expired pokemon so that the seen set is not too large
 	def optional_arguments(self, info):
 		if config['REV_LOC']:
@@ -368,5 +368,5 @@ class Alarm_Manager(Thread):
 			info.update(**get_biking_data(info))
 		if config['DM_DRIVE']:
 			info.update(**get_driving_data(info))
-		
+
 		return info
